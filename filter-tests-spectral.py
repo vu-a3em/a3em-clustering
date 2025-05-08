1161013a2b55bd60cfbe84009bf4ebcf1e5a5a65
Devin Jean <emailcruzjean@yahoo.com>
2025-05-08 15:50:27 -0500

    spectral flux filter

--- filter-tests-spectral.py
+++ filter-tests-spectral.py
@@ -15,8 +15,8 @@ BACKGROUNDS = [
 # -------------------------------------------------------
 
 EVENT_FREQS = [
-    # [0, 'Frog:8', 'Rooster:1', 'Aircraft:1'],
-    [1, 'Frog:1', 'Rooster:8', 'Aircraft:1'],
+    [0, 'Frog:8', 'Rooster:1', 'Aircraft:1'],
+    # [1, 'Frog:1', 'Rooster:8', 'Aircraft:1'],
 ]
 MAX_CLUSTERS = [
     # 8,
@@ -33,79 +33,53 @@ MAX_CLUSTERS = [
     1024,
 ]
 MAX_WEIGHT = [
-    # 8,
+    1,
+    8,
     # 16,
-    32,
+    # 32,
     # 40,
     # 48,
     # 56,
-    64,
+    # 64,
     # 80,
     # 96,
-    128,
+    # 128,
     # 192,
-    256,
-    512,
-    1024,
+    # 256,
+    # 512,
+    # 1024,
 ]
 FILTER_THRESH = [
-    0.050,
-    0.055,
-    0.060,
-    0.065,
-    0.070,
-    0.075,
-    0.080,
-    0.085,
-    0.090,
-    0.095,
-    0.100,
-    # 0.105,
-    # 0.110,
-    # 0.115,
-    # 0.120,
-    # 0.13,
-    # 0.14,
-    # 0.15,
-    # 0.2,
-    # 0.25,
-    # 0.3,
-    # 0.35,
-    # 0.4,
-    # 0.45,
-    # 0.5,
-    # 0.55,
-    # 0.6,
-    # 0.65,
-    # 0.7,
-]
-VOTE_THRESH = [
-    # 0.0,
-    # 0.1,
-    # 0.2,
-    0.3,
-    # 0.4,
-    # 0.5,
-]
-RADIUS = [
-    8,
-    16,
-    # 24,
-    # 32,
-    # 40,
-    # 64,
-    # 128,
-    # 256,
-]
-CHUNKS = [
-    # 0,
-    4,
-    # 8,
-    # 12,
-    # 16,
-    # 20,
-    # 24,
-    # 28,
+    0.05,
+    0.06,
+    0.07,
+    0.08,
+    0.09,
+    0.10,
+    0.11,
+    0.12,
+    0.13,
+    0.14,
+    0.15,
+    0.20,
+    0.30,
+    0.40,
+    0.50,
+    0.60,
+    0.70,
+    0.80,
+    0.90,
+    1.00,
+    1.10,
+    1.20,
+    1.30,
+    1.40,
+    1.50,
+    # 5,
+    # 10,
+    # 100,
+    # 500,
+    # 1000,
 ]
 BACKGROUND_SCALE = [
     # 0.0,
@@ -128,7 +102,7 @@ if __name__ == '__main__':
 
     if not os.path.exists(args.output): os.mkdir(args.output)
 
-    queue = list(itertools.product(EVENT_FREQS, MAX_CLUSTERS, MAX_WEIGHT, FILTER_THRESH, VOTE_THRESH, RADIUS, CHUNKS, BACKGROUND_SCALE))
+    queue = list(itertools.product(EVENT_FREQS, MAX_CLUSTERS, MAX_WEIGHT, FILTER_THRESH, BACKGROUND_SCALE))
     queue_init_size = len(queue)
     queue_lock = threading.Lock()
     def worker():
@@ -138,11 +112,11 @@ if __name__ == '__main__':
                 i = queue_init_size - len(queue)
                 info = queue.pop()
                 print(f'starting task {i + 1}/{queue_init_size}')
-            file = f'{args.output}/b{info[7]}-p{info[6]}-r{info[5]}-v{info[4]}-c{info[1]}-w{info[2]}-t{info[3]}-e{info[0][0]}.txt'
+            file = f'{args.output}/b{info[4]}-c{info[1]}-w{info[2]}-t{info[3]}-e{info[0][0]}.txt'
             if os.path.exists(file): continue
             with open(file, 'w') as f:
                 subprocess.check_call([
-                    'time', 'python', 'sim2.py', '--quiet',
+                    'time', 'python', 'sim2-spectral.py', '--quiet',
                     '--events', *EVENTS,
                     '--backgrounds', *BACKGROUNDS,
                     '--clips', str(round(2 * 60 * args.hours)),
@@ -152,10 +126,7 @@ if __name__ == '__main__':
                     '--max-clusters', str(info[1]),
                     '--max-weight', str(info[2]),
                     '--filter-thresh', str(info[3]),
-                    '--vote-thresh', str(info[4]),
-                    '--radius', str(info[5]),
-                    '--chunks', str(info[6]),
-                    '--background-scale', str(info[7]),
+                    '--background-scale', str(info[4]),
                     *(['--quantized'] if args.quantized else []),
                 ], stdout = f, stderr = f)
     workers = [threading.Thread(target = worker) for _ in range(args.jobs)]
--- sim2-spectral.py
+++ sim2-spectral.py
@@ -4,15 +4,12 @@ import dataloader
 import random
 import math
 import os
-import torch
 import soundfile
-import mfcc_vae_8 as vae
 import mfcc
 import numpy as np
 import filter
-import tensorflow as tf
 
-from typing import Dict, Any
+from typing import Dict, Any, Optional
 
 QUIET = False
 def qprint(*args, **kwargs):
@@ -62,106 +59,37 @@ def create_fade(size, *, fade_duration, sr):
         np.linspace(1, 0, t),
     ])
 
-def energy_peak(x: np.array, *, radius: int):
-    kernel = np.concatenate([
-        np.linspace(0, 1, radius + 2)[1:-1],
-        [1],
-        np.linspace(1, 0, radius + 2)[1:-1],
-    ])
-    return np.argmax(np.convolve(x**2, kernel, mode = 'same'))
-
-def energy_chunks(x: np.array, *, size: int, radius: int, count: int):
-    if count == 0:
-        return np.split(x, len(x) // size)
-
-    res = []
-    x = np.copy(x)
-    half = size // 2
-    e = np.concatenate([np.zeros((half,)), x, np.zeros((half,))])
-    for _ in range(count):
-        p = energy_peak(x, radius = radius)
-        res.append(e[p : p + size])
-        x[max(0, p - half) : min(len(x), p - half + size)] = 0
-    return np.array(res)
-
-def chunks(x: np.array, *, size: int, overlaps: int) -> np.array:
-    assert len(x) >= size
-    p = 0
-    s = round(size / (1 + overlaps))
-    res = []
-    while p + size <= len(x):
-        res.append(x[p:p+size])
-        p += s
-    return np.array(res)
-
-class TFLiteModel:
-    def __init__(self, *args, **kwargs):
-        self.model = tf.lite.Interpreter(*args, **kwargs)
-        self.model.allocate_tensors()
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        self.model.set_tensor(self.model.get_input_details()[0]['index'], x)
-        self.model.invoke()
-        return (torch.tensor(self.model.get_tensor(self.model.get_output_details()[0]['index'])), None)
+class SpectralEncoder:
+    def __init__(self, *, fft_size: Optional[int] = None, sample_rate: int):
+        self.sample_rate = sample_rate
+        self.fft_size = fft_size if fft_size is not None else int(30 / 1000 * sample_rate)
+    def forward(self, x: np.ndarray) -> np.ndarray:
+        return np.abs(np.mean(mfcc.spectrogram(x, fft_size = self.fft_size, sample_rate = self.sample_rate), axis = 0))
 
 if __name__ == '__main__':
-    # x = np.array([1,3,2,3,2,1,4,2,3,1,24,1000,23,21,2,3,1,3,2,1,3])
-    # for c in energy_chunks(x, size = 8, radius = 5, count = 3):
-    #     print(c)
-    # os.abort()
-
     parser = argparse.ArgumentParser()
     parser.add_argument('--events', type = str, nargs = '+', required = True)
     parser.add_argument('--backgrounds', type = str, nargs = '+', required = True)
     parser.add_argument('--seed', type = int)
     parser.add_argument('--clip-duration', type = float, default = 30)
-    parser.add_argument('--clips', type = int, default = 2 * 60 * 24)
+    parser.add_argument('--clips', type = int, default = 10)
     parser.add_argument('--bg-change-prob', type = float, default = 0.01666)
     parser.add_argument('--event-prob', type = float, default = 1.0)
     parser.add_argument('--event-freqs', type = str, nargs = '*', default = [])
     parser.add_argument('--max-clusters', type = int, default = 256)
     parser.add_argument('--max-weight', type = float, default = 1024.0)
     parser.add_argument('--filter-thresh', type = float, default = 0.2)
-    parser.add_argument('--vote-thresh', type = float, default = 0.0)
     parser.add_argument('--audio-out', type = str)
     parser.add_argument('--fade-duration', type = float, default = 1)
     parser.add_argument('--max-silence-ratio', type = float, default = 0.1)
-    parser.add_argument('--radius', type = int, default = 16)
-    parser.add_argument('--chunks', type = int, default = 4)
     parser.add_argument('--background-scale', type = float, default = 1.0)
     parser.add_argument('--iterations', type = int, default = 1)
-    parser.add_argument('--embedding_size', type = int, default = 16)
-    parser.add_argument('--quantized', action = 'store_true')
     parser.add_argument('--quiet', action = 'store_true')
     args = parser.parse_args()
 
     assert args.iterations >= 1
     if args.seed is not None: random.seed(args.seed)
     globals()['QUIET'] = True
-
-    if args.quantized:
-        device = 'cpu'
-        print(f'using tflite quantized model on device "{device}"\n')
-
-        encoder = TFLiteModel(model_path = 'model.tflite')
-    else:
-        device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
-        print(f'using standard model on device "{device}"\n')
-
-        encoder = vae.Encoder(embedding_size = args.embedding_size).to(device)
-        # encoder.load_state_dict(torch.load('mfcc-untested-1/encoder-F16-A0.9-E256-L171.pt', weights_only = True)) # version used in paper
-        # encoder.load_state_dict(torch.load('mfcc-2-untested-1/encoder-F16-A0.999-E256-L33.pt', weights_only = True))
-        # encoder.load_state_dict(torch.load('mfcc-2-untested-2/encoder-F16-A0.95-E256-L34.pt', weights_only = True))
-        # encoder.load_state_dict(torch.load('mfcc-3-untested-1/encoder-F16-A0.95-E256-L34.pt', weights_only = True))
-        # encoder.load_state_dict(torch.load('mfcc-4-untested-1/encoder-F16-A0.95-E256-L38.pt', weights_only = True))
-        # encoder.load_state_dict(torch.load('mfcc-4-untested-3/encoder-F16-A0.95-E256-L36.pt', weights_only = True))
-        # encoder.load_state_dict(torch.load('mfcc-5-untested-4/encoder-F16-A0.95-E256-L34.pt', weights_only = True))
-        # encoder.load_state_dict(torch.load('mfcc-6-untested-2/encoder-F16-A0.95-E256-L28.pt', weights_only = True))
-        # encoder.load_state_dict(torch.load('mfcc-7-untested-1/encoder-F16-A0.9-E256-L29.pt', weights_only = True))
-        # encoder.load_state_dict(torch.load('mfcc-8-untested-1/encoder-F16-A0.9-E256-L28.pt', weights_only = True))
-        # encoder.load_state_dict(torch.load('mfcc-8-untested-2/encoder-F16-A0.9-E256-L27.pt', weights_only = True))
-        # encoder.load_state_dict(torch.load('mfcc-8-untested-3/encoder-F16-A0.8-E256-L28.pt', weights_only = True))
-        encoder.load_state_dict(torch.load('mfcc-8-untested-4/encoder-F16-A0.5-E256-L22.pt', weights_only = True))
-        encoder.eval()
 
     qprint('loading sounds...')
     backgrounds = dict(sum((list(load_sounds(path, min_length = dataloader.SAMPLE_DURATION_SECS, mult_length = dataloader.SAMPLE_DURATION_SECS, max_silence_ratio = args.max_silence_ratio).items()) for path in sorted(args.backgrounds)), start = []))
@@ -196,20 +124,16 @@ if __name__ == '__main__':
     for x in [x for x in events.keys() if x not in event_freqs]:
         del events[x]
 
+    encoder = SpectralEncoder(sample_rate = dataloader.UNIFORM_SAMPLE_RATE)
+
     clips = []
     input_events = { x: 0 for x in [None] + list(event_freqs.keys()) }
     output_events = input_events.copy()
     background_class = None
     clip_len = dataloader.UNIFORM_SAMPLE_RATE * args.clip_duration
+    embedding_size = len(encoder.forward(np.zeros((clip_len,))))
     for i in range(args.iterations):
-        f = filter.ClusterFilter(args.max_clusters, args.max_weight, args.embedding_size, args.filter_thresh)
-        def vote_retain(x: np.ndarray) -> bool:
-            votes = []
-            for seg in energy_chunks(x, size = dataloader.UNIFORM_SAMPLE_RATE * dataloader.SAMPLE_DURATION_SECS, count = args.chunks, radius = args.radius):
-                with torch.no_grad():
-                    mean, _ = encoder.forward(torch.tensor(mfcc.mfcc_spectrogram_for_learning(seg, dataloader.UNIFORM_SAMPLE_RATE)[np.newaxis,:], dtype = torch.float).to(device))
-                    votes.append(f.insert(mean.cpu().numpy().squeeze()))
-            return np.mean(votes) > args.vote_thresh
+        f = filter.ClusterFilter(args.max_clusters, args.max_weight, embedding_size, args.filter_thresh)
 
         for _ in range(args.clips):
             if background_class is None or random.random() < args.bg_change_prob:
@@ -230,7 +154,7 @@ if __name__ == '__main__':
 
             if args.audio_out is not None: clips.append(clip)
             input_events[event_class] += 1
-            if vote_retain(clip): output_events[event_class] += 1
+            if f.insert(encoder.forward(clip)): output_events[event_class] += 1
 
         if args.audio_out is not None:
             p = args.audio_out if args.iterations == 1 else f'{args.audio_out[:args.audio_out.rfind(".")]}-{i}.{args.audio_out[args.audio_out.rfind(".")+1:]}'
